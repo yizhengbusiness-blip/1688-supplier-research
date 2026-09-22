83fac1eabdc78e9d0432f982c46078bfa07068ab
yizhengbusiness-blip <yizhengbusiness@gmail.com>
2026-09-22 18:05:41 +0800

    Create config.py Add project configuration

---
+++ config.py
@@ -0,0 +1,13 @@
+"""Configuration for the 1688 supplier research project."""
+
+SEARCH_KEYWORDS = [
+    "香水包材",
+]
+
+TARGET_REGIONS = [
+    "广东",
+]
+
+MAX_PAGES = 3
+
+OUTPUT_FILE = "output/suppliers.xlsx"
